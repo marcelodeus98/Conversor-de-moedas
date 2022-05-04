846bbf115f6ba5643970700034ace17ac885227d
marcelodeus98 <marcelodeus98@gmail.com>
2022-05-03 21:15:34 -0300

    alter index.js

--- index.js
+++ index.js
@@ -6,50 +6,42 @@ let Converter = () => {
     let valueSelectOne = selectMoedaOne.value;
     let valueSelectTwo = selectMoedaTwo.value;
     let elementConvertido = document.getElementById("valueConvert");
-    console.log(elementConvertido)
-   
 
-    if(valueSelectOne === 'EURO' || valueSelectTwo ==='REAL'){
-        let obterValue = valueMoeda*5.24;
-        let valueFinal = obterValue.toFixed(2);
+    if(valueSelectOne == 'EURO' && valueSelectTwo == 'REAL'){
+        let valueFinal = valueMoeda*5.24
         let passValueConvertido = `O valor convertido é : ${valueFinal}`
         elementConvertido.innerHTML = passValueConvertido;
     }
 
-    if(valueSelectOne === 'EURO' || valueSelectTwo ==='DOLAR'){
-        let obterValue = valueMoeda*1.10;
-        let valueFinal = obterValue.toFixed(2);
+    if(valueSelectOne == 'EURO' && valueSelectTwo == 'DOLAR'){
+        let obterValue = valueMoeda*1.10
         let passValueConvertido = `O valor convertido é : ${valueFinal}`
         elementConvertido.innerHTML = passValueConvertido;
 
     }
 
-    if(valueSelectOne === 'REAL' || valueSelectTwo ==='DOLAR'){
-        let obterValue = valueMoeda*0.21;
-        let valueFinal = obterValue.toFixed(2);
+    if(valueSelectOne == 'REAL' && valueSelectTwo ==='DOLAR'){
+        let valueFinal = valueMoeda*0.21
         let passValueConvertido = `O valor convertido é : ${valueFinal}`
         elementConvertido.innerHTML = passValueConvertido;
 
     }
 
-    if(valueSelectOne === 'REAL' || valueSelectTwo ==='EURO'){
-        let obterValue = valueMoeda*0.19;
-        let valueFinal = obterValue.toFixed(2);
+    if(valueSelectOne == 'REAL' && valueSelectTwo == 'EURO'){
+        let valueFinal = valueMoeda*0.19
         let passValueConvertido = `O valor convertido é : ${valueFinal}`
         elementConvertido.innerHTML = passValueConvertido;
     }
 
-    if(valueSelectOne === 'DOLAR' || valueSelectTwo ==='REAL'){
-        let obterValue = valueMoeda*4.77;
-        let valueFinal = obterValue.toFixed(2);
+    if(valueSelectOne == 'DOLAR' && valueSelectTwo == 'REAL'){
+        let valueFinal = valueMoeda*4.77
         let passValueConvertido = `O valor convertido é : ${valueFinal}`
         elementConvertido.innerHTML = passValueConvertido;
 
     }
 
-    if(valueSelectOne === 'DOLAR' || valueSelectTwo ==='EURO'){
-        let obterValue = valueMoeda*0.91;
-        let valueFinal = obterValue.toFixed(2);
+    if(valueSelectOne == 'DOLAR' &&  valueSelectTwo == 'EURO'){
+        let valueFinal = valueMoeda*0.91
         let passValueConvertido = `O valor convertido é : ${valueFinal}`
         elementConvertido.innerHTML = passValueConvertido;
 
