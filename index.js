b17856c7caec99d1aa9cc3bd5d7327f0b0041633
marcelodeus98 <marcelodeus98@gmail.com>
2022-07-03 07:18:35 -0300

    alter readme

--- index.js
+++ index.js
@@ -9,40 +9,40 @@ let Converter = () => {
 
     if(valueSelectOne == 'EURO' && valueSelectTwo == 'REAL'){
         let valueFinal = valueMoeda*5.24
-        let passValueConvertido = `O valor convertido é : ${valueFinal}`
+        let passValueConvertido = `O valor convertido é : ${valueFinal.toFixed(2)}`
         elementConvertido.innerHTML = passValueConvertido;
     }
 
     if(valueSelectOne == 'EURO' && valueSelectTwo == 'DOLAR'){
         let valueFinal = valueMoeda*1.10
-        let passValueConvertido = `O valor convertido é : ${valueFinal}`
+        let passValueConvertido = `O valor convertido é : ${valueFinal.toFixed(2)}`
         elementConvertido.innerHTML = passValueConvertido;
     }
    
 
     if(valueSelectOne == 'REAL' && valueSelectTwo ==='DOLAR'){
-        let valueFinal = valueMoeda*0.21
-        let passValueConvertido = `O valor convertido é : ${valueFinal}`
+        let valueFinal = valueMoeda * 0.19 
+        let passValueConvertido = `O valor convertido é : ${valueFinal.toFixed(2)}`
         elementConvertido.innerHTML = passValueConvertido;
 
     }
 
     if(valueSelectOne == 'REAL' && valueSelectTwo == 'EURO'){
         let valueFinal = valueMoeda*0.19
-        let passValueConvertido = `O valor convertido é : ${valueFinal}`
+        let passValueConvertido = `O valor convertido é : ${valueFinal.toFixed(2)}`
         elementConvertido.innerHTML = passValueConvertido;
     }
 
     if(valueSelectOne == 'DOLAR' && valueSelectTwo == 'REAL'){
         let valueFinal = valueMoeda*4.77
-        let passValueConvertido = `O valor convertido é : ${valueFinal}`
+        let passValueConvertido = `O valor convertido é : ${valueFinal.toFixed(2)}`
         elementConvertido.innerHTML = passValueConvertido;
 
     }
 
     if(valueSelectOne == 'DOLAR' &&  valueSelectTwo == 'EURO'){
         let valueFinal = valueMoeda*0.91
-        let passValueConvertido = `O valor convertido é : ${valueFinal}`
+        let passValueConvertido = `O valor convertido é : ${valueFinal.toFixed(2)}`
         elementConvertido.innerHTML = passValueConvertido;
 
     }
